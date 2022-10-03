f702a9d78e48ff26f77fc86bbbd859449274a89c
Mohammad Ibrahim <awesomecoder.dev@gmail.com>
2022-10-03 21:11:36 +0600

    Start Up Project

---
+++ pages/404.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Image from 'next/future/image'
+
+class notFound extends Component {
+    render() {
+        return (
+            <div>
+                404
+            </div>
+        );
+    }
+}
+
+export default notFound;
---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
