498653d82453bd31f868432f42adcc315b3b051e
Mohammad Ibrahim <awesomecoder.dev@gmail.com>
2022-10-04 15:29:06 +0600

    Start Up

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,12 +25,12 @@ class Hero extends Component {
                         </div>
                         <div className="relative flex py-10">
                             <Link href="#" >
-                                <a className="bg-primary-500 text-slate-600 text-lg font-semibold py-3 rounded-xl px-10">
+                                <a className="bg-primary-500 mx-2 text-slate-600 text-lg font-semibold py-3 rounded-xl px-10">
                                     Get started free
                                 </a>
                             </Link>
                             <Link href="#" >
-                                <a className="text-slate-600 text-lg font-semibold py-3 px-10 flex items-center justify-between">
+                                <a className="bg-white mx-2 border border-slate-400/10 rounded-xl text-slate-600 text-lg font-semibold py-3 px-10 flex items-center justify-between">
                                     <PlayCircleIcon strokeWidth={1} stroke="currentColor" className="w-8 mr-2" />
                                     <span>Play intro</span>
                                 </a>
@@ -38,9 +38,9 @@ class Hero extends Component {
                         </div>
                     </div>
                     <div className="relative px-14 py-10 ">
-                        <div className="h-4 w-4 absolute rounded-full bg-orange-400 pointer-events-none"></div>
-                        <div className="transform -rotate-12 h-6 w-6 right-20 top-0 absolute bg-orange-400 pointer-events-none"></div>
-                        <div className="transform -rotate-45 h-4 w-4 right-28 top-32 absolute opacity-70 bg-sky-400 z-10 pointer-events-none"></div>
+                        <div className="h-4 w-4 absolute left-36 rounded-full bg-orange-400 pointer-events-none"></div>
+                        <div className="transform -rotate-12 h-6 w-6 right-10 top-0 absolute bg-orange-400 pointer-events-none"></div>
+                        <div className="transform -rotate-45 h-4 w-4 right-12 top-32 absolute opacity-70 bg-sky-400 z-10 pointer-events-none"></div>
                         <div className="transform overflow-hidden -rotate-12 pointer-events-none absolute top-44 z-10 text-center bg-white border border-slate-200/50 w-44 h-24 rounded-2xl shadow-xl">
                             <h2 className='text-sm p-2 font-semibold text-slate-500'>Completed</h2>
                             <div className="flex-1 space-y-2 py-1 p-4">
@@ -62,10 +62,10 @@ class Hero extends Component {
                                 />
                             </div> */}
                         </div>
-                        <div className="h-7 w-7 absolute bottom-8 left-48 rounded-full shadow-xl flex justify-center items-center bg-white pointer-events-none z-10">
+                        <div className="h-7 w-7 absolute bottom-8 left-56 rounded-full shadow-xl flex justify-center items-center bg-white pointer-events-none z-10">
                             <div className="bg-green-400 h-3 w-3 rounded-full"></div>
                         </div>
-                        <div className="h-2 w-2 absolute bottom-14 transform -rotate-12  left-24 bg-sky-400 pointer-events-none"></div>
+                        <div className="h-2 w-2 absolute bottom-14 left-28 transform -rotate-12 bg-sky-400 pointer-events-none"></div>
 
 
                         <div className="relative flex justify-end items-center">
